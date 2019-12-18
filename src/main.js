96ae5e7547c45d6698bba035ced44eb70d42dd8b
qi1582189593 <1582189593@qq.com>
2019-12-18 22:45:50 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui' // 引入模块
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less' // 引入初始化样式
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)// 注册全局elementul
 new Vue({
   router,
   render: h => h(App)
