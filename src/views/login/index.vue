2e28b47564f9168aa742efecfd3f5d204d424a72
qi1582189593 <1582189593@qq.com>
2019-12-19 11:59:32 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,24 +6,24 @@
        <div class='title'>
         <img src="../../assets/img/logo_index.png" alt="">
        </div>
-       <!-- 放置表单 -->
-       <el-form>
+       <!-- 放置表单,绑定表单数据对象,绑定数据的校验规则-->
+       <el-form :model="loginForm" :rules="loginRules" ref="myForm">
           <!-- 表单域 里面   放置 input/select/checkbox 相当于一行-->
-          <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+          <el-form-item prop="mobile" >
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号" ></el-input>
           </el-form-item>
           <!-- 表单域 -->
-          <el-form-item>
-          <el-input style="width:65%" placeholder="验证码"></el-input>
+          <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:65%" placeholder="验证码"></el-input>
           <el-button style="float:right" type="primary" plain>发送验证码</el-button>
           </el-form-item>
           <!-- 复选框 -->
-          <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+          <el-form-item prop="agree">
+          <el-checkbox v-model="loginForm.agree">我已阅读并同意用户协议和隐私条款</el-checkbox>
           </el-form-item>
           <!-- 登录按钮 -->
           <el-form-item>
-          <el-button style="width:100%" type="primary">登录</el-button>
+          <el-button style="width:100%" type="primary" @click="submitLogin">登录</el-button>
           </el-form-item>
 
        </el-form>
@@ -33,7 +33,58 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // 绑定表单数据对象
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        agree: false // 是否同意协议
+
+      },
+      loginRules: {
+        //   决定着校验规则  key(字段名):value(对象数组) => 一个对象就是一个校验规则
+        // required 为true 就表示该字段必填 如果不填 就会提示消息
+        mobile: [
+          { required: true, message: '请输入手机号码' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不对' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码为6位' }
+        ],
+        agree: [
+          { validator: function (rule, value, callback) {
+            // 自定义校验函数
+          // rule 规则 没啥用
+          // value 要校验的字段的值
+          // callback 是一个回调函数
+            // if (value) {
+            //   // 认为已经勾选
+            //   callback()// 认为当前的规则校验通过了
+            // } else {
+            //   callback(new Error('必须同意用户协议'))// 如果没有勾选 认为当前校验失败 应该停止
+            // }
+            // 三元表达式
+            value ? callback() : callback(new Error('必须同意用户协议'))
+          } }
+        ]
+
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          //  说明校验通过  应该调用登录接口
+          console.log('校验通过,开始调用登录接口')
+        }
+      })
+    }
+  }
+}
 
 </script>
 
