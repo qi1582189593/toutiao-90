c8c03291887c41be58813b55e46a25991f8c314c
qi1582189593 <1582189593@qq.com>
2019-12-19 10:11:51 +0800

    登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>登录页面</div>
+  <div class="login">
+    <!--使用elemenent框架-->
+    <el-card class="longin-vard">
+      <!-- 放置背景图片-->
+       <div class='title'>
+        <img src="../../assets/img/logo_index.png" alt="">
+       </div>
+       <!-- 放置表单 -->
+       <el-form>
+          <!-- 表单域 里面   放置 input/select/checkbox 相当于一行-->
+          <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+          </el-form-item>
+          <!-- 表单域 -->
+          <el-form-item>
+          <el-input style="width:65%" placeholder="验证码"></el-input>
+          <el-button style="float:right" type="primary" plain>发送验证码</el-button>
+          </el-form-item>
+          <!-- 复选框 -->
+          <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+          </el-form-item>
+          <!-- 登录按钮 -->
+          <el-form-item>
+          <el-button style="width:100%" type="primary">登录</el-button>
+          </el-form-item>
+
+       </el-form>
+
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,5 +38,24 @@ export default {}
 </script>
 
 <style lang='less' scoped>
+.login{
+ background-image: url(../../assets/img/back.jpg);
+ height: 100vh;
+ background-size: cover;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+  .longin-vard{
+    width: 440px;
+    height: 350px;
+      .title{
+        text-align:center;
+        margin-bottom: 30px;
+          img{
+            height: 45px;
+          }
+      }
+  }
+}
 
 </style>
