eab615544e23a0d419ab805b70e919e54497a4f5
qi1582189593 <1582189593@qq.com>
2019-12-21 11:57:12 +0800

    统一全局注册插件的使用方式Vue.use

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入模块
+import Component from './components'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less' // 引入初始化样式
 import axios from 'axios'
@@ -9,6 +10,7 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 设置一个常态值
 Vue.prototype.$axios = axios // 赋值给全局对象
 Vue.use(ElementUI)// 注册全局elementul
+Vue.use(Component) // 全局注册
 new Vue({
   router,
   render: h => h(App)
