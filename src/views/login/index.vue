da75d6da8b9bc30cfa74dd13b7c974e361bdb028
qi1582189593 <1582189593@qq.com>
2019-12-21 09:44:31 +0800

    登录模块-登录请求-token存储问题

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -79,7 +79,17 @@ export default {
       this.$refs.myForm.validate(function (isOK) {
         if (isOK) {
           //  说明校验通过  应该调用登录接口
-          console.log('校验通过,开始调用登录接口')
+          this.$axios({
+            url: '/authorizations', // 请求地址 axios 没有指定 类型 默认走get类型
+            method: 'post', // 类型
+            data: this.loginForm // body 参数
+          }).then(result => {
+            // 只接受正确结果
+            // 前端缓存 登录成功返回给我们的令牌
+            window.localStorage.setItem('user-token', result.data.data.token)
+          }).catch(() => {
+
+          })
         }
       })
     }
