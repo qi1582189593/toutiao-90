2c218477ad02275354bcd5b1cd4f64c190bed054
qi1582189593 <1582189593@qq.com>
2019-12-22 09:59:31 +0800

    统一处理响应数据

--- src/permission/index.js
+++ src/permission/index.js
@@ -2,7 +2,7 @@
 // 处理路由拦截器 导航守卫
 import router from '../router'
 router.beforeEach(function (to, from, next) {
-  console.log(to.fullPath)
+  // console.log(to.fullPath)
   if (to.fullPath.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
--- src/utils/request.js
+++ src/utils/request.js
@@ -12,5 +12,34 @@ axios.interceptors.request.use(function (config) {
   // 执行请求错误
 
 })
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 成功时执行该函数 状态码 200 /201 /204
 
+  return response.data ? response.data : {}
+}, function () {
+  // 失败时执行该函数
+  // let status = error.response.status
+  // let message = ''
+  // switch (status) {
+  //   case 400:
+  //     message = '请求参数错误'
+  //     break
+  //   case 507:
+  //     message = '服务器数据库异常'
+  //     break
+  //   case 401:
+  //     // token过期或者失效
+  //     // this.$router
+  //     window.localStorage.removeItem('user-token') // 删除过期的token
+  //     router.push('/login') // 跳转到登录页
+  //     break
+  //   case 403:
+  //     message = '没有设置这条评论的权限'
+  //     break
+  //   default:
+  //     break
+  // }
+  // Message({ type: 'warning', message }) // 提示信息
+})
 export default axios
